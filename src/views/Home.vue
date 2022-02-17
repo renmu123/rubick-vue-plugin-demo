76eb46c9f97f5872912f619771707fa7934bdf52
renmu123 <1101022351@qq.com>
2022-02-17 15:19:15 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    hello Rubick
+    <button id="showNotification">通知</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  // components: {
+  //   HelloWorld,
+  // },
+  mounted() {
+    document
+      .getElementById("showNotification")
+      .addEventListener("click", () => {
+        window.showNotification();
+      });
   },
 };
 </script>
